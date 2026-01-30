ea0a1d928cd72b90896d3ee5e0f26a8043b24281
Naman Katiyar <namankatiyar120@gmail.com>
2026-01-30 10:53:29 +0530

    Add Project Files

---
+++ schema.js
@@ -0,0 +1,38 @@
+const Joi = require('joi');
+
+
+//Schema for listings
+module.exports.listingSchema = Joi.object({
+    listing : Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().allow("", null),
+        category: Joi.string()
+        .valid(
+        "featured",
+        "rooms",
+        "iconic_cities",
+        "mountains",
+        "castles",
+        "amazing_pools",
+        "camping",
+        "farms",
+        "arctic",
+        "domes",
+        "boats"
+        )
+        .required(),
+    }).required(),
+});
+
+
+//Schema for Reviews
+module.exports.reviewSchema = Joi.object({
+    review : Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        comment: Joi.string().required(),
+    }).required(),
+});
