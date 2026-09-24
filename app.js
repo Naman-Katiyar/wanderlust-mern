6c9e4a3482b5154bc9775076f018979b3a055f51
Your Name <your.email@example.com>
2026-09-24 16:40:16 +0530

    Fix app startup and redirect root to listings

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV != "production") {
-    require('dotenv').config();
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
 
 const express = require("express");
@@ -17,7 +17,7 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo").default;
 const flash = require("connect-flash");
 const passport = require("passport");
-const LocalStrategy = require("passport-local"); 
+const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
 const listingRouter = require("./routes/listing.js");
@@ -27,83 +27,75 @@ const userRouter = require("./routes/user.js");
 //const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
 
-main().then(() => {
+main()
+  .then(() => {
     console.log("Connected to DB");
-}).catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 async function main() {
-    await mongoose.connect(dbUrl);
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-
 const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    crypto: {
-        secret: process.env.SECRET,
-    },
-    touchAfter: 24 * 3600,
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
 });
 
 store.on("error", () => {
-    console.log("ERROR in MONGO SESSION STORE", err);
+  console.log("ERROR in MONGO SESSION STORE", err);
 });
 
 const sessionOptions = {
-    store,
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 3,
-        maxAge: 1000 * 60 * 60 * 24 * 3,
-        httpOnly: true,
-    }
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 3,
+    maxAge: 1000 * 60 * 60 * 24 * 3,
+    httpOnly: true,
+  },
 };
-
 
 // app.get("/", (req, res) => {
 //     res.send("Hi, I'm root!!");
 // });
 
-
-
 app.use(session(sessionOptions));
 app.use(flash());
 
-app.use(passport.initialize()); 
+app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
-    //console.log(res.locals.success);
-
-    res.locals.selectedCategory = req.query.category || "featured";
-    res.locals.searchQuery = req.query.search || "";
-    next();
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  res.locals.selectedCategory = req.query.category || "featured";
+  res.locals.searchQuery = req.query.search || "";
+  next();
 });
 
-// Global search query (for navbar)
-app.use((req, res, next) => {
-    res.locals.searchQuery = req.query.search || "";
-    next();
+app.get("/", (req, res) => {
+  res.redirect("/listings");
 });
-
 
 // app.get("/demouser", async (req, res) => {
 //     let fakeUser = new User ({
@@ -115,13 +107,12 @@ app.use((req, res, next) => {
 //     res.send(registeredUser);
 // })
 
-
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
 app.use((req, res, next) => {
-    next(new ExpressError(404, "Page NOT Found"));
+  next(new ExpressError(404, "Page NOT Found"));
 });
 
 // app.all("*", (req, res, next) => {
@@ -129,11 +120,16 @@ app.use((req, res, next) => {
 // });
 
 app.use((err, req, res, next) => {
-    let {statusCode=500, message="Something went wrong!"} = err;
-    res.status(statusCode).render("error.ejs", {message});
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  res.locals.selectedCategory = req.query.category || "featured";
+  res.locals.searchQuery = req.query.search || "";
 
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message });
 });
 
 app.listen(8080, () => {
-    console.log("App is listening on port 8080");
+  console.log("App is listening on port 8080");
 });
